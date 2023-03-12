f8ba5ba7a57382cd8f5c7df994febb3c989a1fb2
amal khanjer <amlkh2724@gmail.com>
2023-03-12 13:41:11 +0200

    node

---
+++ ex1-node/hello.js
@@ -0,0 +1,13 @@
+// Access to system resources: Node.js provides a way for JavaScript to interact with the file system, network, and other system resources that are not available in the browser environment. This allows Node.js to be used for building server-side applications and command-line tools that require access to these resources.
+
+// Built-in modules: Node.js comes with a set of built-in modules that provide functionality for common tasks such as working with the file system, processing HTTP requests, and interacting with databases. These modules are not available in client-side JavaScript because they are designed specifically for server-side use.
+
+// Non-blocking I/O: Node.js uses a non-blocking I/O model that allows it to handle large numbers of concurrent connections efficiently. This is important for server-side applications that need to handle a high volume of requests without blocking other requests.
+
+// Three things client-side JavaScript has that Node.js doesn't have are:
+
+// Access to the DOM: Client-side JavaScript can manipulate the Document Object Model (DOM) of a web page to create dynamic and interactive user interfaces. This is not possible in Node.js because it does not have access to the DOM.
+
+// Browser APIs: Client-side JavaScript can use browser APIs to interact with user input, display graphics and media, and perform other tasks that are specific to the browser environment. These APIs are not available in Node.js because it is designed for server-side use.
+
+// Cross-platform compatibility: Client-side JavaScript can run on any device that has a web browser, regardless of the operating system or hardware. Node.js, on the other hand, is designed to run on specific platforms and may not be compatible with all operating systems and hardware configurations.
